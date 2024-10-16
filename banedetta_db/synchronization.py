5417041da58ff0b391171be79e5264dc6d85d801
Taskov1ch <tynaev.acc@gmail.com>
2024-10-16 19:10:37 +0600

    ...

--- banedetta_db/synchronization.py
+++ banedetta_db/synchronization.py
@@ -7,14 +7,14 @@ class DataSynchronizer:
 	db: DB
 	platform: str
 
-	async def synchronization(self):
+	async def synchronize_problems(self):
 		while True:
-			for data in await self.db.get_no_posts_bans(self.platform):
-				data["problem"] = "no_post"
-				yield data
-
-			for data in await self.db.get_resolved_bans(self.platform):
-				data["problem"] = "resolved"
-				yield data
+			async for data, problem in (
+				(await self.db.get_no_posts_bans(self.platform), "no_post"),
+				(await self.db.get_resolved_bans(self.platform), "resolved"),
+			):
+				for d in data:
+					d["problem"] = problem
+					yield d
 
 			await sleep(3)
